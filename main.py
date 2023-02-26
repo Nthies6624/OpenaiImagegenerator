cd49767996a8c63df069a8378b1434c5832ed6b2
Noah Thies <thiesnoah@hotmail.com>
2023-02-25 22:09:45 -0600

    Working on bug with not displaying pic

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 import requests
 import PIL
 from flask import Flask, render_template, request, send_from_directory
+from flask import send_file
 import openai
 import os
+my_secret = os.environ['Openai key']
+
+
 
 app = Flask(__name__, template_folder='templates')
 
 # Set up OpenAI API key
-openai.api_key = "Your key"
+openai.api_key = my_secret
 
 # Create route to serve index.html
 @app.route('/')
 def index():
     return render_template('index.html')
+  
+@app.route('/generated-images/<path:filename>')
+def serve_generated_image(filename):
+    return send_file(f'images/{filename}', mimetype='image/png')
 
 @app.route('/script.js')
 def serve_script():
-  return send_from_directory('static', 'script.js')
+    return send_from_directory('static', 'script.js')
 
 # Create route to handle form submission
 @app.route('/generate-image', methods=['POST'])
 def generate_image():
-
     # Get user input from the form
     image_size = request.form.get('image-size')
     bg_color = request.form.get('background-color')
@@ -40,9 +47,13 @@ def generate_image():
     img_path = f"images/{img_filename.split('.')[0]}.png"
     with open(img_path, 'wb') as f:
         f.write(requests.get(img_url).content)
-
+      
     # Serve the generated image to the front-end
     return send_from_directory('images', f"{img_filename.split('.')[0]}.png")
+  
+    # Return path to generated image
+    return render_template('index.html', image=img_path)
+    return
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='60', debug=True)
